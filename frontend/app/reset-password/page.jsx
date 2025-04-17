2c491e543c0f171c8fef6da769a41f92d0ef2816
sutariyadeep123 <sutariyadeep123@gmail.com>
2025-04-17 03:14:25 -0400

    last one

--- frontend/app/reset-password/page.jsx
+++ frontend/app/reset-password/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { UserCircle, Lock } from 'lucide-react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
@@ -15,7 +15,7 @@ export default function ResetPassword() {
     async function delay(time) {
         await new Promise((resolve) => setTimeout(resolve, time));
     }
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -34,61 +34,63 @@ export default function ResetPassword() {
             window.location.href = '/login';
         } catch (error) {
             console.log(error)
-            toast.error(error.response.data.error.message||"Failed to reset password. Please try again.");
+            toast.error(error.response.data.error.message || "Failed to reset password. Please try again.");
             console.error("Error resetting password:", error);
         }
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-                <div className="flex flex-col items-center mb-6">
-                    <UserCircle className="w-20 h-20 text-blue-800 mb-2" />
-                    <h2 className="text-2xl font-semibold text-blue-800">Reset Password</h2>
+        <Suspense fallback="Loading...">
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+                <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+                    <div className="flex flex-col items-center mb-6">
+                        <UserCircle className="w-20 h-20 text-blue-800 mb-2" />
+                        <h2 className="text-2xl font-semibold text-blue-800">Reset Password</h2>
+                    </div>
+                    <form onSubmit={handleSubmit} className="space-y-4">
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
+                                New Password
+                            </label>
+                            <div className="relative">
+                                <input
+                                    type="password"
+                                    id="password"
+                                    className="w-full p-2 pl-10 border rounded focus:ring-blue-500 focus:border-blue-500"
+                                    placeholder="Enter new password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                                <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700 mb-1">
+                                Confirm New Password
+                            </label>
+                            <div className="relative">
+                                <input
+                                    type="password"
+                                    id="confirmPassword"
+                                    className="w-full p-2 pl-10 border rounded focus:ring-blue-500 focus:border-blue-500"
+                                    placeholder="Confirm new password"
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required
+                                />
+                                <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+                        >
+                            Reset Password
+                        </button>
+                    </form>
                 </div>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                    <div>
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
-                            New Password
-                        </label>
-                        <div className="relative">
-                            <input
-                                type="password"
-                                id="password"
-                                className="w-full p-2 pl-10 border rounded focus:ring-blue-500 focus:border-blue-500"
-                                placeholder="Enter new password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700 mb-1">
-                            Confirm New Password
-                        </label>
-                        <div className="relative">
-                            <input
-                                type="password"
-                                id="confirmPassword"
-                                className="w-full p-2 pl-10 border rounded focus:ring-blue-500 focus:border-blue-500"
-                                placeholder="Confirm new password"
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                required
-                            />
-                            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
-                        </div>
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
-                    >
-                        Reset Password
-                    </button>
-                </form>
             </div>
-        </div>
+        </Suspense>
     );
 }
