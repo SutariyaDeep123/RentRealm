bac2679a1658a1ac90ebf7b1b776bd1a17b22d93
SutariyaDeep123 <sutariyadeep123@gmail.com>
2025-04-17 10:50:00 -0400

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: ['localhost'], // Add your backend domain
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '5000',
-                pathname: '/**',
-            },
-        ],
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rentrealm-backend.onrender.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
