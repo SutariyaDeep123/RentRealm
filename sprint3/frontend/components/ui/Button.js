a3a268cecf14bc865249c29053b398109e7a1744
SutariyaDeep123 <sutariyadeep123@gmail.com>
2025-04-17 02:13:55 -0400

    Delete sprint3 directory

--- sprint3/frontend/components/ui/Button.js
+++
@@ -1,5 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export default function Button({ children,className,...props }) {
-    return <button className={cn("bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300",className)} {...props}>{children}</button>
-}
